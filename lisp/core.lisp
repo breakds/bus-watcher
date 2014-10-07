83609a21823c23a5c57a2aeca3398da402fb9ec9
Break Yang <BreakDS@gmail.com>
2014-10-07 08:07:47 -0400

    add json parser

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -10,12 +10,36 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defparameter +query-prefix+ "http://realtime.mbta.com/developer/api/v2/"))
 
+;; This key allows 10000 requests per day
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter +mbta-key+ "52-0DcawcEiYlX0P0hLmYw")) 
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter *route-table* nil))
+
 (defmacro construct-query (query-type &rest query-parameters)
   `(let ((*print-case* :downcase))
      (format nil "~a~a?~{~a=~a~^&~}" ,+query-prefix+ ,query-type 
-	     ',query-parameters)))
+	     '(,@query-parameters 
+               :api_key ,+mbta-key+
+               :format "json"))))
 
-;; (defun request-route (route-id)
-;;   (let ((query (format nil "~a
+(defmacro do-query (query-type &rest query-parameters)
+  (with-gensyms (response)
+    `(let ((,response (drakma:http-request
+                       (construct-query ,query-type
+                                        ,@query-parameters))))
+       (jsown:parse (binary-to-string ,response)))))
+
+(defun binary-to-string (binary-vector)
+  (coerce (loop for code across binary-vector
+             collect (code-char code))
+          'string))
+
+(defun prediction-by-route (route)
+  (do-query :predictionbyroute 
+    :route route))
+
+  
 
 
