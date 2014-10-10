db09d3293287c99cb204c7c3b49527497976a925
BreakDS <breakds@gmail.com>
2014-10-10 11:03:56 +0800

    transfer

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -14,9 +14,6 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defparameter +mbta-key+ "52-0DcawcEiYlX0P0hLmYw")) 
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defparameter *route-table* nil))
-
 (defmacro construct-query (query-type &rest query-parameters)
   `(let ((*print-case* :downcase))
      (format nil "~a~a?~{~a=~a~^&~}" ,+query-prefix+ ,query-type 
@@ -30,6 +27,38 @@
                        (construct-query ,query-type
                                         ,@query-parameters))))
        (jsown:parse (binary-to-string ,response)))))
+
+(defun update-route-table ()
+  (let ((result (do-query "routes"))
+	(route-id-table (make-hash-table :test #'equal)))
+    (loop for route-set in (jsown:val result "mode")
+       do (loop for route in (jsown:val route-set "route")
+	     do (let ((route-id (jsown:val route "route_id"))
+		      (route-name (jsown:val route "route_name")))
+		  (setf (gethash route-id route-id-table) route-name))))
+    route-id-table))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter *route-table* nil))
+
+(defun init ()
+  (setf *route-table* (update-route-table)))
+
+(defun look-up-route-id (route-id)
+  (gethash route-id *route-table*))
+
+(defun look-up-route-name (route-name)
+  (loop 
+     for id being the hash-keys of *route-table*
+     for name being the hash-values of *route-table*
+     when (equal name route-name)
+     return id))
+
+(defun look-up-route (designate)
+  "Returns the corresponding route-id. DESIGNATE can be either a route-id or a route-name"
+  (if (look-up-route-id designate)
+      designate
+      (look-up-route-name designate)))
 
 (defun binary-to-string (binary-vector)
   (coerce (loop for code across binary-vector
